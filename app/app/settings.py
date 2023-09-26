032b5f4c570e8a7a8ea30f2e65902782cca31019
ssuunaj <jamex027@gmail.com>
2023-09-26 18:57:58 +0200

    added api-documentation

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
-]
+    'core',
+    'rest_framework',
+    'drf_spectacular',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -130,3 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK =   {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
